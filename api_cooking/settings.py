0b06d82f85a273017c83d4cec07379f4f5081f3b
Timur <mr14mr@yandex.ru>
2019-07-20 17:36:58 +0300

    set AWS_DEFAULT_ACL = None

--- api_cooking/settings.py
+++ api_cooking/settings.py
@@ -132,6 +132,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME', 'cooking-media')
 AWS_AUTO_CREATE_BUCKET = True
+AWS_DEFAULT_ACL = None
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
 
 django_heroku.settings(locals())
