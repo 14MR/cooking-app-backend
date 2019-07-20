be95849d265fd176e996df15c682559aadc58a26
Timur <mr14mr@yandex.ru>
2019-07-20 17:32:46 +0300

    added s3 storage

--- api_cooking/settings.py
+++ api_cooking/settings.py
@@ -126,4 +126,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "files", "media")
 MEDIA_URL = "/media/"
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME', 'cooking-media')
+AWS_AUTO_CREATE_BUCKET = True
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
+
 django_heroku.settings(locals())
